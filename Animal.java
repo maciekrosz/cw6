8271d58b6747b278b4ce5bb3ce137f47e9faef1e
MACIEK <s33498@pjwstk.edu.pl>
2025-04-23 16:06:26 +0200

    zadanie 1,2

---
+++ Animal.java
@@ -0,0 +1,43 @@
+
+    class Animal {
+        public void sleep() {
+            System.out.println("Zwierzę śpi...");
+        }
+
+        public void makeNoise() {
+            System.out.println("Zwierzę wydaje dźwięk.");
+        }
+
+        public void roam() {
+            System.out.println("Zwierzę wędruje...");
+        }
+    }
+
+
+    class Lion extends Animal {
+        @Override
+        public void makeNoise() {
+            System.out.println("Lew ryczy!");
+        }
+    }
+
+    class Cat extends Animal {
+        @Override
+        public void makeNoise() {
+            System.out.println("Kot miauczy!");
+        }
+    }
+
+    class Wolf extends Animal {
+        @Override
+        public void makeNoise() {
+            System.out.println("Wilk wyje!");
+        }
+    }
+
+    class Dog extends Animal {
+        @Override
+        public void makeNoise() {
+            System.out.println("Pies szczeka!");
+        }
+    }
